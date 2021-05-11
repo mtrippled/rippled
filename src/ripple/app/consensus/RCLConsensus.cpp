40cdddee10aefae07992a7ad111674057700f289
Mark Travis <mtravis@ripple.com>
2021-05-11 10:57:33 -0700

    Trace masterlock.

--- src/ripple/app/consensus/RCLConsensus.cpp
+++ src/ripple/app/consensus/RCLConsensus.cpp
@@ -637,9 +637,12 @@ RCLConsensus::Adaptor::doAccept(
         auto timer7 = perf::START_TIMER(tracer_);
 
         // Build new open ledger
-        std::unique_lock lock{app_.getMasterMutex(), std::defer_lock};
+//        std::unique_lock lock{*app_.getMasterMutex(), std::defer_lock};
+        perf::unique_lock lock(*app_.getMasterMutex(), FILE_LINE,
+                               std::defer_lock);
         std::unique_lock sl{ledgerMaster_.peekMutex(), std::defer_lock};
-        std::lock(lock, sl);
+//        std::lock(lock, sl);
+        perf::lock(lock, sl, FILE_LINE);
         perf::END_TIMER(tracer_, timer7);
 
         auto timer8 = perf::START_TIMER(tracer_);
