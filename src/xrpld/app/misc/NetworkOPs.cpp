3511ad7cc92bff0998e68a680a76997ff7c51426
Mark Travis <mtravis@ripple.com>
2024-08-07 16:44:46 -0700

    Log proposals and validations.

--- src/xrpld/app/misc/NetworkOPs.cpp
+++ src/xrpld/app/misc/NetworkOPs.cpp
@@ -2333,6 +2333,18 @@ NetworkOPsImp::recvValidation(
     lock.unlock();
 
     pubValidation(val);
+    std::stringstream ss;
+    ss << "VALIDATION: " << val->render() << " master_key: ";
+    auto master = app_.validators().getTrustedKey(val->getSignerPublic());
+    if (master)
+    {
+        ss << toBase58(TokenType::NodePublic, *master);
+    }
+    else
+    {
+        ss << "none";
+    }
+    JLOG(m_journal.debug()) << ss.str();
 
     // We will always relay trusted validations; if configured, we will
     // also relay all untrusted validations.
