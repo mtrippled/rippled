b07a17bc5f5affefd6561a2fca7ff17a2c76f409
Mark Travis <mtravis@ripple.com>
2024-09-16 12:39:31 -0700

    Add to account_objects RPC.

--- include/xrpl/protocol/jss.h
+++ include/xrpl/protocol/jss.h
@@ -552,6 +552,7 @@ JSS(peers);                       // out: InboundLedger, handlers/Peers, Overlay
 JSS(peer_disconnects);            // Severed peer connection counter.
 JSS(peer_disconnects_resources);  // Severed peer connections because of
                                   // excess resource consumption.
+JSS(permissioned_domain);         // out: AccountObjects
 JSS(port);                        // in: Connect, out: NetworkOPs
 JSS(ports);                       // out: NetworkOPs
 JSS(previous);                    // out: Reservations
